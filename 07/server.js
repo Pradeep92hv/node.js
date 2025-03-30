f05bbe8f45baec52b445aedc172a714b9d0ef92f
Pradeep  H V <hvpradeep92@gmail.com>
2025-03-30 13:27:27 +0530

    middleware1 ( for form , json, static folder)  2:30 hrs

---
+++ 07/server.js
@@ -0,0 +1,73 @@
+console.log('server running!!!')
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+
+
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:  
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.get('^/$|/index(.html)?',(req,res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+})
+
+
+
+app.get('/new-page(.HTML)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    // res.redirect('/new-page.html');
+    res.redirect(301, '/new-page.html'); //302 by default
+});
+
+// Route handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.send('Hello World!');
+});
+
+// chaining route handlers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server running on port ${POR
